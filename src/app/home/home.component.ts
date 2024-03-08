763caee6ce9d96e7185d7e57bf2264be166da4a2
Kumar MA <public.kumar@gmail.com>
2024-03-07 22:05:32 -0800

    153e

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { AuthService } from '../auth.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+  
 export class HomeComponent implements OnInit {
 
   constructor(private router: Router, private authService: AuthService) { }
