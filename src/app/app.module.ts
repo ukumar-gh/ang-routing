f5a84fcdc7a263476ddcaa6947fd02e7aee1e00b
Kumar MA <public.kumar@gmail.com>
2024-03-20 20:53:12 -0700

    App module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
